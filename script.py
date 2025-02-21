8912c1cec53e6b22bf0fbbc6b7d8baf0967bb1c7
thecoder <fathas235@gmail.com>
2025-02-21 20:05:36 +0200

    create amount and total functions and respective variables

--- script.py
+++ script.py
@@ -1,7 +1,19 @@
+def calcAmount(percent,salary):
+  return(percent / 100) * salary
+
+def totalAmount(savings_amount, rent_amount, electricity_amount):
+  return(savings_amount + rent_amount + electricity_amount)
+
+
 month = input("Enter the month: ")
 salary = float(input("Enter your salary: "))
 percent = input("Enter the % for your Savings, Rent and Elecricity separated by a comma: ")
 
 savings, rent, electricity = [float(value) for value in percent.split(",")]
 
-print(savings, rent, electricity)
+savings_amount = calcAmount(savings, salary)
+rent_amount = calcAmount(rent, salary)
+electricity_amount = calcAmount(electricity, salary)
+
+print(calcAmount(savings, salary))
+print(totalAmount(savings_amount, rent_amount, electricity_amount))
