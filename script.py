61dfa28f882b119f753e9c86579a052d0e8f4d01
thecoder <fathas235@gmail.com>
2025-02-21 20:20:08 +0200

    create square salary, real remainder functions and remainder variable

--- script.py
+++ script.py
@@ -10,12 +10,17 @@ def calcRemainder(salary, sum_amount):
 def calcYearly(rent_amount, electricity_amount):
   return(rent_amount * 12, electricity_amount * 12)
 
+def squareSalary(salary):
+  return salary ** 2
+
+def realRamainder(remainder):
+  return remainder - 50
 
 month = input("Enter the month: ")
-salary = float(input("Enter your salary: "))
+salary = int(input("Enter your salary: "))
 percent = input("Enter the % for your Savings, Rent and Elecricity separated by a comma: ")
 
-savings, rent, electricity = [float(value) for value in percent.split(",")]
+savings, rent, electricity = [int(value) for value in percent.split(",")]
 
 savings_amount = calcAmount(savings, salary)
 rent_amount = calcAmount(rent, salary)
@@ -23,4 +28,4 @@ electricity_amount = calcAmount(electricity, salary)
 
 sum_amount = totalAmount(savings_amount, rent_amount, electricity_amount)
 
-print(calcYearly(rent_amount, electricity_amount))
+remainder = calcRemainder(salary, sum_amount)
