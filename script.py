49e45eb3a578d81ff4c0490fbc54efd669db2f07
thecoder <fathas235@gmail.com>
2025-02-21 20:44:30 +0200

    create script function and store result in a dictionary

--- script.py
+++ script.py
@@ -16,16 +16,34 @@ def squareSalary(salary):
 def realRamainder(remainder):
   return remainder - 50
 
-month = input("Enter the month: ")
-salary = int(input("Enter your salary: "))
-percent = input("Enter the % for your Savings, Rent and Elecricity separated by a comma: ")
+def createScript():
+  month = input("Enter the month: ")
+  salary = int(input("Enter your salary: "))
+  percent = input("Enter the % for your Savings, Rent and Elecricity separated by a comma: ")
 
-savings, rent, electricity = [int(value) for value in percent.split(",")]
+  savings, rent, electricity = [int(value) for value in percent.split(",")]
 
-savings_amount = calcAmount(savings, salary)
-rent_amount = calcAmount(rent, salary)
-electricity_amount = calcAmount(electricity, salary)
+  savings_amount = calcAmount(savings, salary)
+  rent_amount = calcAmount(rent, salary)
+  electricity_amount = calcAmount(electricity, salary)
 
-sum_amount = totalAmount(savings_amount, rent_amount, electricity_amount)
+  sum_amount = totalAmount(savings_amount, rent_amount, electricity_amount)
+  remainder = calcRemainder(salary, sum_amount)
+  real_remainder = realRamainder(remainder)
 
-remainder = calcRemainder(salary, sum_amount)
+
+
+  result = {
+          "Month": month,
+          "Salary": salary,
+          "Savings amount": savings_amount,
+          "Rent amount": rent_amount,
+          "Electricity amount": electricity_amount,
+          "Total of all 3": sum_amount,
+          "Remainder of Salary": remainder,
+          "Yearly rent and Yearly electricity respectively": calcYearly(rent_amount, electricity_amount),
+          "Square salary": squareSalary(salary),
+          "Real remainder": real_remainder
+      }
+
+  return result
